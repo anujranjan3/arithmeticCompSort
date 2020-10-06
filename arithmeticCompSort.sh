adc6cf541d2767cdc2dda6d52596b747d153f1d9
unknown <ararky7@gmail.com>
2020-10-06 16:32:42 +0530

    Computation a + b * c is done

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -2,8 +2,13 @@
 
 read -p "Enter the value of a : " a;
 echo "You have entered :" $a "for a";
+
 read -p "Enter the value of b : " b;
 echo "You have entered :" $b "for b";
+
 read -p "Enter the value of c : " c;
 echo "You have entered :" $c "for c";
 
+echo "Computing a + b * c"
+compute=$(($a+$b*$c));
+echo "After Doing Computation "$a" + "$b" *" $c" we got "$compute;
