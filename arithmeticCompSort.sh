af569a439beb2b28c0d90937f30c418c68ca042a
unknown <ararky7@gmail.com>
2020-10-06 17:05:30 +0530

    Results for Every Computation stored in Dictionary

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+declare -A computeDict;
+i=0;
+
 read -p "Enter the value of a : " a;
 echo "You have entered :" $a "for a";
 
@@ -9,6 +12,30 @@ echo "You have entered :" $b "for b";
 read -p "Enter the value of c : " c;
 echo "You have entered :" $c "for c";
 
+storingDict () {
+	compute=$1;
+	computeDict[(($i))]=$compute;
+	(( i++ ));
+}
+
+echo "Computing a + b * c"
+compute=$(($a+$b*$c));
+storingDict $compute;
+echo "After Doing Computation "$a" + "$b" *" $c" we got "$compute;
+
+echo "Computing a * b + c"
+compute=$(($a*$b+$c));
+storingDict $compute;
+echo "After Doing Computation "$a" * "$b" +" $c" we got "$compute
+
+echo "Computing c + a / b"
+compute=$(($c+$a/$b));
+storingDict $compute;
+echo "After Doing Computation "$c" + "$a" /" $b" we got "$compute;
+
 echo "Computing a % b + c"
 compute=$(($a%$b+$c));
+storingDict $compute;
 echo "After Doing Computation "$a" % "$b" +" $c" we got "$compute;
+
+echo "All The Compuations are stored in Dictionary : "${computeDict[@]};
