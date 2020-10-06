6f9f7e2af56d523d33c30f0cd6de2d64a8bf4013
unknown <ararky7@gmail.com>
2020-10-06 16:26:23 +0530

    Program to get three input values

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
-echo "Welcome to Arithmetic Computation and sorting Program"
+read -p "Enter the value of a : " a;
+echo "You have entered :" $a "for a";
+read -p "Enter the value of b : " b;
+echo "You have entered :" $b "for b";
+read -p "Enter the value of c : " c;
+echo "You have entered :" $c "for c";
+
