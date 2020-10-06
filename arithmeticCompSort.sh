5eb039d0d7591e1535493e756e72e3c3441df9eb
unknown <ararky7@gmail.com>
2020-10-06 19:51:25 +0530

    Computation result in descending order

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -18,6 +18,20 @@ storingDict () {
 	computeArray[i]=${computeDict[i]};
 	(( i++ ));
 }
+sorting () {
+	for (( j=0; j<$i; j++ ))
+	do
+		for (( k=$j+1; k<$i; k++ ))
+		do
+			if [ ${computeArray[j]} -lt ${computeArray[k]} ]
+			then
+				temp=${computeArray[j]};
+				computeArray[j]=${computeArray[k]};
+				computeArray[k]=$temp;
+			fi
+		done
+	done
+}
 
 echo "Computing a + b * c"
 compute=$(($a+$b*$c));
@@ -41,3 +55,7 @@ echo "After Doing Computation "$a" % "$b" +" $c" we got "$compute;
 
 echo "All The Compuations are stored in Dictionary : "${computeDict[@]};
 echo "All the computation are retrieved from dictionary to array :"${computeArray[*]};
+
+echo "Sorting Array in Descending Order "
+sorting;
+echo "Computations are sorted in Descending order :"${computeArray[*]};
